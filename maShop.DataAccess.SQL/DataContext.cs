83660202a5b0a0377eb513548c1f4b7ee449a8da
pandoraciel <86033350+pandoraciel@users.noreply.github.com>
2021-08-10 02:11:57 +0700

    entity framework

---
+++ maShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using maShop.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ProductCategory> ProductCategory { get; set; }
+    }
+}
