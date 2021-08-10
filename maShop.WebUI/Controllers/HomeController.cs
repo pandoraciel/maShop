c7a126b23a6874f4e91cae69a70f876225379c7f
pandoraciel <86033350+pandoraciel@users.noreply.github.com>
2021-08-11 03:59:18 +0700

    Product List

--- maShop.WebUI/Controllers/HomeController.cs
+++ maShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using maShop.core.Contracts;
+using maShop.core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,26 @@ namespace maShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            //context = new InMemoryRepository<Product>();
+            context = productContext;
+            //productCategories = new InMemoryRepository<ProductCategory>();
+            productCategories = productCategoryContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Detail(string Id)
+        {
+            Product product = context.Find(Id);
+            return View(product);
         }
 
         public ActionResult About()
