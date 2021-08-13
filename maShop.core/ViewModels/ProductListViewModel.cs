6291c0ecf1fd2474c9442bc487f0cb91c11f838b
pandoraciel <86033350+pandoraciel@users.noreply.github.com>
2021-08-14 04:00:16 +0700

    Product Filter

---
+++ maShop.core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using maShop.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maShop.core.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
