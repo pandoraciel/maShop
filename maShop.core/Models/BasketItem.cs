c88ee84f27de9ac8d9cf23d770baaa6be247e6b5
pandoraciel <86033350+pandoraciel@users.noreply.github.com>
2021-08-14 04:30:13 +0700

    add Basket

---
+++ maShop.core/Models/BasketItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maShop.core.Models
+{
+    public class BasketItem : BaseEntity
+    {
+        public string BasketId { get; set; }
+        public string ProductId { get; set; }
+        public string Qty { get; set; }
+    }
+}
